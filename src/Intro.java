fbf2e27ce75c50b0d7a919d22ece531bb9526e2d
MikeZorin <mykhailo.zorin@gmail.com>
2021-04-19 11:29:33 -0500

    added methods to Rectangle class

--- src/Intro.java
+++ src/Intro.java
@@ -5,7 +5,21 @@ class Rectangle {
 	private int _width;
 	private boolean _filled;
 	private double _area;
-	private String _color = "red";
+	private String _color = "white";
+	
+	public void setWidth(int value) {
+		
+		if(value < 0) {
+			System.err.println("Incorrect value for Height");
+		}
+		else {
+			_width = value;
+		}
+	}
+	
+	public int getWidth() {
+		return _width;
+	}
 	
 	public void setHeight(int value) {
 		
@@ -25,12 +39,31 @@ class Rectangle {
 		return _filled;
 	}
 	
+	public void setIsFilled(boolean value) {
+		_filled = value;
+	}
+	
+	
 	public String getColor() {
 		return _color;
 	}
 	
 	public double getArea() {
 		return _area;
+	}
+	
+	public void draw() {
+		
+		for (int y = 0; y < _height; y++) {
+			for (int x = 0; x < _width; x++) {
+
+				boolean b = 
+						!(y == 0 || y == _height - 1 || x == 0 || x == _width - 1) 
+						&& !_filled;
+				System.out.print( b ? ' ' : '*' );
+			}
+			System.out.println();
+		}
 	}
 }
 
@@ -39,19 +72,14 @@ public class Intro {
 	public static void main(String[] args) {
 		
 		Rectangle r1 = new Rectangle();
-		// r1.height = -77;
-		r1.setHeight(-77);
-		System.out.println( "Height = " + r1.getHeight() );
-		System.out.println( "Filled = " + r1.getIsFilled() );
-		System.out.println( "Color = " + r1.getColor() );
-		System.out.println( "Area = " + r1.getArea() );
-		// r1.width = -22;
+		r1.setHeight(5);
+		r1.setWidth(15);
+		r1.setIsFilled(true);
 		
-		// Rectangle #1
-		int height1 = 5;
-		int width1 = 15;
-		boolean filled1 = true;
-		//Rectangle(height1, width1, filled1);
+		r1.draw();
+		
+		// Rectangle(r1.getHeight(), r1.getWidth(), r1.getIsFilled());
+		// Rectangle(r1);
 	
 		// Rectangle #2
 		int height2 = 7;
@@ -64,6 +92,22 @@ public class Intro {
 		int width3 = 32;
 		boolean filled3 = true;
 		//Rectangle(9, 32, true);
+	}
+	
+	public static void Rectangle(Rectangle r) {
+		
+		int h = r.getHeight();
+		int w = r.getWidth();
+		boolean isFilled = r.getIsFilled();
+		
+		for (int y = 0; y < h; y++) {
+			for (int x = 0; x < w; x++) {
+
+				boolean b = !(y == 0 || y == h - 1 || x == 0 || x == w - 1) && !isFilled;
+				System.out.print( b ? ' ' : '*' );
+			}
+			System.out.println();
+		}
 	}
 	
 	public static void Rectangle(int h, int w, boolean isFilled) {
